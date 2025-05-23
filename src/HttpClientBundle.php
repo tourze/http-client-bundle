ea402a6f2cb0eba907d8dc9c1418b78d850353ea
github-ci <you@example.com>
2025-05-23 11:58:58 +0000

    feat(tls): 更新依赖并添加README和LICENSE文件

--- src/HttpClientBundle.php
+++ src/HttpClientBundle.php
@@ -5,12 +5,20 @@ namespace HttpClientBundle;
 use HttpClientBundle\DependencyInjection\Compiler\RemoveUnusedServicePass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tourze\BundleDependency\BundleDependencyInterface;
 
-class HttpClientBundle extends Bundle
+class HttpClientBundle extends Bundle implements BundleDependencyInterface
 {
     public function build(ContainerBuilder $container): void
     {
         parent::build($container);
         $container->addCompilerPass(new RemoveUnusedServicePass());
+    }
+
+    public static function getBundleDependencies(): array
+    {
+        return [
+            \Tourze\Symfony\RuntimeContextBundle\RuntimeContextBundle::class => ['all' => true],
+        ];
     }
 }
