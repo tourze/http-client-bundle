058de1e078bc3e4277a9d12701ecc00951ba2b0e
github-ci <you@example.com>
2025-06-15 19:57:00 +0800

    新增多个 Doctrine Bundle 依赖以扩展功能，并更新依赖配置：

--- src/HttpClientBundle.php
+++ src/HttpClientBundle.php
@@ -6,6 +6,15 @@ use HttpClientBundle\DependencyInjection\Compiler\RemoveUnusedServicePass;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
 use Tourze\BundleDependency\BundleDependencyInterface;
+use Tourze\DoctrineAsyncInsertBundle\DoctrineAsyncInsertBundle;
+use Tourze\DoctrineIndexedBundle\DoctrineIndexedBundle;
+use Tourze\DoctrineIpBundle\DoctrineIpBundle;
+use Tourze\DoctrineTimestampBundle\DoctrineTimestampBundle;
+use Tourze\DoctrineUserAgentBundle\DoctrineUserAgentBundle;
+use Tourze\DoctrineUserBundle\DoctrineUserBundle;
+use Tourze\ScheduleEntityCleanBundle\ScheduleEntityCleanBundle;
+use Tourze\Symfony\AopAsyncBundle\AopAsyncBundle;
+use Tourze\Symfony\RuntimeContextBundle\RuntimeContextBundle;
 
 class HttpClientBundle extends Bundle implements BundleDependencyInterface
 {
@@ -18,7 +27,15 @@ class HttpClientBundle extends Bundle implements BundleDependencyInterface
     public static function getBundleDependencies(): array
     {
         return [
-            \Tourze\Symfony\RuntimeContextBundle\RuntimeContextBundle::class => ['all' => true],
+            DoctrineAsyncInsertBundle::class => ['all' => true],
+            DoctrineIndexedBundle::class => ['all' => true],
+            DoctrineIpBundle::class => ['all' => true],
+            DoctrineTimestampBundle::class => ['all' => true],
+            DoctrineUserBundle::class => ['all' => true],
+            DoctrineUserAgentBundle::class => ['all' => true],
+            RuntimeContextBundle::class => ['all' => true],
+            ScheduleEntityCleanBundle::class => ['all' => true],
+            AopAsyncBundle::class => ['all' => true],
         ];
     }
 }
